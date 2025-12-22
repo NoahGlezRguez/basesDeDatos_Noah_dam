3c04f3087175f119baeab26299bd251e2718ad13
Noah González Rodríguez <noah.gonzalezro@elorrieta-errekamari.com>
2025-12-22 01:39:41 +0100

    terminado ej 1 tema 8

--- Organizado por temas de clase/Tema 8/ejercicio 1 tema 8.sql
+++ Organizado por temas de clase/Tema 8/ejercicio 1 tema 8.sql
@@ -44,51 +44,32 @@ having avg(salario) > 1800;
 
 
 /*e) Visualiza el número de vendedores del departamento llamado Ventas.*/
+select count(*) 'nº trabajadores de ventas'
+from empleado e natural join departamento d
+where d.nomdep = 'Ventas';
 
-
+	-- otra opcion seria:
+select count(*) 'nº trabajadores de ventas'
+from empleado e natural join departamento d
+group by d.nomdep
+having d.nomdep = 'Ventas';
 
 /*f) Indica para todos los empleados que trabajan en el departamento de ventas su nombre, salario,
 comisión, el nombre de su jefe (columna Jefe), el salario de este (columna Salario jefe) y el
 resultado de dividir el salario del jefe entre el del empleado (columna Factor multiplicador)
 redondeado a dos decimales.*/
-
+select d.nomdep, e.nomemp 'empleado', e.salario 'salario empleado', e.comision, j.nomemp 'jefe', j.salario 'salario jefe', round(j.salario/e.salario,2) 'Factor multiplicador'
+from departamento d join empleado e on d.numdep = e.numdep left join empleado j on e.numempjefe = j.numemp 
+where d.nomdep = 'Ventas';
 
 
 /*g) Indica para todos los empleados que tengan dos o más empleados subordinados, su nombre,
 salario, número de subordinados que tiene y el nombre del departamento en el que trabaja.
 Ordena el resultado por número de subordinados de mayor a menor.*/
 
+select j.nomemp, j.salario, count(e.numemp) 'nº subordinados', d.nomdep
+from  empleado j join empleado e on j.numemp = e.numempjefe join departamento d on j.numdep = d.numdep
+group by  j.numemp, j.nomemp, j.salario, d.nomdep
+having count(e.numempjefe) >= 2
+order by count(e.numemp) desc; 
 
-
-/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-/* EJERCICIO 2.-*/
-use empresa2_dam;
-
-/*a) Muestra por cada centro de trabajo, su nombre y dirección, así como el número de
-departamentos en que se divide y el presupuesto máximo y mínimo de sus departamentos.*/
-
-
-
-/*b) Muestra por cada habilidad su código y descripción, el número de empleados que la poseen y
-el nivel medio de dicha habilidad redondeado a un decimal.*/
-
-
-
-/*c) Muestra por cada departamento su nombre, presupuesto, el nombre del departamento del que
-depende y el presupuesto de este. Asigna alias a todos los atributos.*/
-
-
-/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-/* EJERCICIO 3.-*/
-/*use geografia;*/
-
-
-/*a) Obtén los nombres de las comunidades autónomas uniprovinciales, es decir, las formadas por
-una sola provincia.*/
-
-
-
-/*b) Muestra por cada provincia, su nombre, su superficie, su población y su densidad poblacional
-redondeada a 2 decimales (columna Densidad poblacional). Este último dato se calcula
-dividiendo la población de la provincia entre su superficie. Ordena los datos por provincias
-desde la menos densamente poblada hasta la más densamente poblada.*/
