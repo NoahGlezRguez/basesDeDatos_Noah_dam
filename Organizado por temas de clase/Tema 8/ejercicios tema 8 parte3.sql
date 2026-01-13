79a6131f9be4054dd1f80ac6d88a86e7d6cb683f
Noah González Rodríguez <noah.gonzalezro@elorrieta-errekamari.com>
2026-01-13 18:59:27 +0100

    new: esqueleto script de entrega y script de creacion bd alumnado_dam

---
+++ Organizado por temas de clase/Tema 8/ejercicios tema 8 parte3.sql
@@ -0,0 +1,40 @@
+/* Entrega parte 3 del tema 8*/
+/*
+Tema 8_PARTE II Actividades --> Consultas (Correlacionadas, Unión ,Intersección y diferencia, Creación de Consultas)
+Ejercicio 1  entrega Script con consultas  (f, g, h)
+Ejercicio 4   entrega Script con consultas  (a, b, c)*/
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+/* del enunciado de la parte 2:
+	Ejercicio 1 - apartados f, g, h.*/
+use empresa_dam;
+
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+/*del enunciado de la parte 2:
+	Ejercicio 4 - apartados a, b, c*/
+    
+use alumnado_dam;
+
+/*4. Se dispone de una base de datos llamada Alumnado con información acerca de los alumnos de un
+centro de enseñanza. Concretamente disponemos de 3 tablas, todas ellas con el mismo esquema
+relacional:
+ Alumnos (DNI, Nombre, Edad, Localidad), con datos de los alumnos actuales del centro.
+ Antiguos (DNI, Nombre, Edad, Localidad), con datos de los alumnos antiguos del centro.
+ Nuevos (DNI, Nombre, Edad, Localidad), con datos de los alumnos que ya se han
+matriculado para el próximo curso.
+Lleva a cabo las siguientes consultas empleando el lenguaje SQL:*/
+
+/*a) Obtén los DNI y nombres de todos los alumnos acerca de los cuales disponemos de datos, es
+decir, de todos los que aparecen en cualquiera de las tres tablas, sin que aparezcan filas
+repetidas en el resultado.*/
+
+
+/*b) Obtén de dos maneras distintas los DNI y nombres de todos los alumnos que aparecen
+simultáneamente en las tres tablas.*/
+
+
+/*c) Obtén de tres maneras diferentes los DNI y nombres de todos los alumnos matriculados este
+curso, pero que no se matricularon en cursos pasados ni están matriculados para el próximo
+curso.*/
+
