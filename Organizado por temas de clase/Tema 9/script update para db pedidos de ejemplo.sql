15327fc48619c97f149fe5e3dc01e4517c05c8e8
Noah González Rodríguez <noah.gonzalezro@elorrieta-errekamari.com>
2026-01-15 20:10:38 +0100

    Update script update para db pedidos de ejemplo.sql

--- Organizado por temas de clase/Tema 9/script update para db pedidos de ejemplo.sql
+++ Organizado por temas de clase/Tema 9/script update para db pedidos de ejemplo.sql
@@ -3,3 +3,17 @@ use pedidos_dam;
 UPDATE Articulo
 SET PVPArt = PVPArt + PVPArt*5/100
 WHERE PVPArt < 1;
+
+UPDATE Pedido
+SET FecPed = CURRENT_DATE
+WHERE RefPed IN (SELECT RefPed FROM LineaPedido);
+
+select * from pedido;
+
+update pedido
+set fecped = '2025-02-08'
+where refped = 'P0001';
+
+insert into pedido values ('P0005', '2025-08-11');
+
+select * from pedido order by fecped;
